305c82a0aff111c93aac1e5bb0b5dc6f68fb3922
Ricardo Vieira <57718264+ricardo4457@users.noreply.github.com>
2025-04-17 18:27:35 +0100

    first commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use App\Models\User;
+use App\Http\Requests\AuthRequest;
+use App\Http\Requests\UserRequest;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\JsonResponse;
+
+
+
+class AuthController extends Controller
+{
+
+    public function login(AuthRequest $request)
+    {
+        try {
+            $credentials = $request->only('username', 'password');
+
+            if (Auth::attempt($credentials)) {
+
+                $request->session()->regenerate();
+
+                return response()->json([
+                    'message' => 'Login successful',
+                    'user' => Auth::user(),
+                ]);
+            }
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Login failed',
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+    public function logout()
+    {
+        Auth::logout();
+
+        return response()->json([
+            'message' => 'Logout successful',
+        ]);
+    }
+
+    public function register(UserRequest $request): JsonResponse
+    {
+        $validatedData = $request->validated();
+
+        try {
+
+            $user = User::create([
+                'username' => $validatedData['username'],
+                'password' => Hash::make($validatedData['password']),
+            ]);
+
+            return response()->json([
+                'message' => 'User registered successfully',
+                'user' => $user,
+            ], 201);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Registration failed',
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TaskCrudController;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    // Route::apiResource('tasks', TaskCrudController::class);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
