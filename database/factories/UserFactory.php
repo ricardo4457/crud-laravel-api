da29d55c11558f71a5ea99b5f1fe974c532bcaa0
Ricardo Vieira <57718264+ricardo4457@users.noreply.github.com>
2025-04-18 12:04:09 +0100

    feat : create task and user factories use it in seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -6,9 +6,6 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
- */
 class UserFactory extends Factory
 {
     /**
@@ -24,21 +21,20 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
+            'username' => $this->faker->unique()->userName,
             'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'created_at' => now(),
+            'updated_at' => now(),
         ];
     }
 
     /**
-     * Indicate that the model's email address should be unverified.
+     * Configure the model factory to use a specific password.
      */
-    public function unverified(): static
+    public function withPassword(string $password): static
     {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
+        return $this->state(fn(array $attributes) => [
+            'password' => Hash::make($password),
         ]);
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Task;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,11 +14,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $users = User::factory()
+            ->count(10)
+            ->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'username' => 'admin',
+            'password' => bcrypt('admin123')
         ]);
+
+        $users->each(function ($user) {
+            Task::factory()
+                ->count(5) 
+                ->create(['user_id' => $user->id]);
+        });
+
+        Task::factory()
+            ->count(20)
+            ->create();
     }
 }
