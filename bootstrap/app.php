a79ea6596ba6b86717e2df4a1a7005fad7e1a879
Ricardo Vieira <57718264+ricardo4457@users.noreply.github.com>
2025-04-18 16:09:56 +0100

    feat : create a schedule to seek expiration date of the token

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Foundation\Application;
+use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
@@ -11,6 +12,11 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
+
+
+->withSchedule(function (Schedule $schedule) {
+    $schedule->command('sanctum:prune-expired ==hours=24')->daily();
+})
     ->withMiddleware(function (Middleware $middleware) {
         //
     })
