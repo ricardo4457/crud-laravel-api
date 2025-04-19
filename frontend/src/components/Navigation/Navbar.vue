77768b91dac7f270b32b7861c1ddcb900ca92e36
Ricardo Vieira <57718264+ricardo4457@users.noreply.github.com>
2025-04-19 13:13:57 +0100

    feat : create a navbar and allow content to fit th rest of page

---
+++ frontend/src/components/Navigation/Navbar.vue
@@ -0,0 +1,35 @@
+<template>
+<nav class="navbar navbar-expand-lg navbar-light bg-light w-100">
+    <div class="container-fluid">
+      <RouterLink to="/" class="navbar-brand">Tasks</RouterLink>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <RouterLink to="/" class="nav-link">Home</RouterLink>
+          </li>
+          <li class="nav-item">
+            <RouterLink to="/register" class="nav-link">Register</RouterLink>
+          </li>
+          <li class="nav-item">
+            <RouterLink to="/login" class="nav-link">Login</RouterLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
