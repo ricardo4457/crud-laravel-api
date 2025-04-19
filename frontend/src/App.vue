7f6e5fb34dad5b4d417d28af62a59c0475982b88
Ricardo Vieira <57718264+ricardo4457@users.noreply.github.com>
2025-04-19 11:51:49 +0100

    feat : remove unneeded components

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,13 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <h1>Full-Stack app of tasks</h1>
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
