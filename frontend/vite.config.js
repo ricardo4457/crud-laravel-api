06ab94589930b194234f7c57f98ca08f33f639f2
Ricardo Vieira <57718264+ricardo4457@users.noreply.github.com>
2025-04-19 12:23:17 +0100

    feat : install bootstrapp 5

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,6 +10,13 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "bootstrap/scss/bootstrap";`
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
