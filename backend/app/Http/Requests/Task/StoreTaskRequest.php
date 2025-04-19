c51fba92d9003b8e5c50ff932bdbdc1a4576a5e9
Ricardo Vieira <57718264+ricardo4457@users.noreply.github.com>
2025-04-19 20:41:51 +0100

    feat : put title as unique

--- backend/app/Http/Requests/Task/StoreTaskRequest.php
+++ backend/app/Http/Requests/Task/StoreTaskRequest.php
@@ -22,7 +22,7 @@ class StoreTaskRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'title' => 'required|string',
+            'title' => 'required|string|unique:tasks',
             'description' => 'required|string',
         ];
     }
