cb21ce3d599470fc3b0ddda03c81c1037e86be58
Ricardo Vieira <57718264+ricardo4457@users.noreply.github.com>
2025-04-19 12:10:14 +0100

    dev : prepare frontend to accept backend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -16,6 +16,17 @@ export default defineConfig({
     },
   },
   server: {
-    port:3000,
-  }
+    port: 3000,
+    open: true,
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:8000",
+        changeOrigin: true,
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      },
+    },
+  },
 })
