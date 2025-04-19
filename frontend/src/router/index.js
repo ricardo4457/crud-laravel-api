db5e667d11dc9adacc3bce11ac00a88ceec6fcf2
Ricardo Vieira <57718264+ricardo4457@users.noreply.github.com>
2025-04-19 14:20:36 +0100

    feat : creating routes for  tasks and Login dummy views

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,18 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 import RegisterView from '../views/Auth/RegisterView.vue' 
+import LoginView from '../views/Auth/LoginView.vue'
+import TaskView from '../views/TaskHomeView.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      name: 'login',
+      component: LoginView
     },
     {
       path: '/register',
       name: 'register',
       component: RegisterView
+    },
+    {
+      path: '/task',
+      name: 'task',
+      component: TaskView
     },
   ],
 })
