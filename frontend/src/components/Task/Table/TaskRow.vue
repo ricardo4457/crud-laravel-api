3c2cff85d652b479abc7e85ffe268021f6db8e0b
Ricardo Vieira <57718264+ricardo4457@users.noreply.github.com>
2025-04-20 22:13:49 +0100

    feat : use date format  helper in table row

--- frontend/src/components/Task/Table/TaskRow.vue
+++ frontend/src/components/Task/Table/TaskRow.vue
@@ -1,6 +1,8 @@
 <template>
   <tr>
     <td>{{ task.title }}</td>
+    <td>{{ formatDate(task.timestamp.created_at, DATE_FORMAT) }}</td>
+    <td>{{ formatDate(task.timestamp.updated_at, DATE_FORMAT) }}</td>
     <td>
       <ActionMenu :actions="actions" @action-click="handleAction" />
     </td>
@@ -10,9 +12,17 @@
 <script setup>
 import ActionMenu from '@/components/Task/Table/ActionMenu.vue'
 import { useRouter } from 'vue-router'
-
+import formatDate from '@/helpers/formatDate'
 const router = useRouter()
 const emit = defineEmits(['action-delete'])
+
+const DATE_FORMAT = {
+  year: 'numeric',
+  month: 'short',
+  day: 'numeric',
+  hour: '2-digit',
+  minute: '2-digit',
+}
 
 const props = defineProps({
   task: {
