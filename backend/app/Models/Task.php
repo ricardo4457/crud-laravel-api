89145ecc7a3f5686de638ea3890d856f9e6c27aa
Ricardo Vieira <57718264+ricardo4457@users.noreply.github.com>
2025-04-20 18:09:37 +0100

    feat :  testing logs on db

--- backend/app/Models/Task.php
+++ backend/app/Models/Task.php
@@ -4,11 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\Activitylog\LogOptions;
 
 class Task extends Model
 {
 
     use HasFactory;
+    use LogsActivity;
 
      protected $fillable = [
          'title',
@@ -16,6 +19,13 @@ class Task extends Model
          'user_id',
        ];
 
+       public function getActivitylogOptions(): LogOptions
+       {
+           return LogOptions::defaults()
+               ->logOnly(['title', 'description' , 'user_id'])
+               ->logOnlyDirty()
+               ->dontSubmitEmptyLogs();
+       }
     public function user()
     {
         return $this->belongsTo(User::class);
