0efe2327def64ab5679e1ababe3b54ecaf32f0c1
Ricardo Vieira <57718264+ricardo4457@users.noreply.github.com>
2025-04-20 22:22:30 +0100

    feat : small fix

--- backend/app/Http/Requests/Task/StoreTaskRequest.php
+++ backend/app/Http/Requests/Task/StoreTaskRequest.php
@@ -22,7 +22,7 @@ class StoreTaskRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'title' => 'required|string|unique:tasks',
+            'title' => 'required|string',
             'description' => 'required|string',
         ];
     }
